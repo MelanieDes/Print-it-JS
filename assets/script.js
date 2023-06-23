0408da8a6bcbde3dc5e899795e228b78cf9d227c
Mélanie Fauchard <melaniefauchard0@gmail.com>
2023-06-23 08:01:30 +0200

    creation de lanimation des fleche et chnagement d'images

--- assets/script.js
+++ assets/script.js
@@ -1,35 +1,48 @@
-//const slides = [
-//	{
-//		"image":"slide1.jpg",
-//		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-//	},
-//	{
-//		"image":"slide2.jpg",
-//    	"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-//	},
-//	{
-//		"image":"slide3.jpg",
-//		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-//	},
-//	{
-//		"image":"slide4.png",
-//		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-//	}
-//]
+const slides = [
+  {
+    image: "slide1.jpg",
+    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+  },
+  {
+    image: "slide2.jpg",
+    tagLine:
+      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+  },
+  {
+    image: "slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "slide4.png",
+    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+  },
+];
 
+const imageSlide = document.querySelector('#banner > img');
+const left = document.querySelector(".arrow_left");
+const right = document.querySelector(".arrow_right");
+const totalSlides = slides.length;
 
-//const slide = ['slide1.jpg', 'slide2.jpg', 'slide3.jpg', 'slide4.png']
-const left = document.querySelector('.arrow_left');
-const right = document.querySelector('.arrow_right');
+let i = 0;
 
-left.addEventListener('click', () => {
-    if (pos > 1){
-        
+function slideShow() {
+    imageSlide.src = `./assets/images/slideshow/${slides[i].image}`;
+};
+
+right.addEventListener("click", () => {
+    if(i == totalSlides -1) {
+        i = 0;
+    }else{
+        i++;
     }
+    slideShow();
 });
 
-
-
-
-
-
+left.addEventListener("click", () => {
+    if(i == 0) {
+        i = totalSlides -1;
+    } else {
+        i--;
+    }
+    slideShow();
+});
