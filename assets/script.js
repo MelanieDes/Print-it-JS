0b8d5d92ba6fefec58a07f9fa7cbac0851b9d9c0
Mélanie Fauchard <melaniefauchard0@gmail.com>
2023-07-14 11:04:41 +0200

    Exercice refait entièrement ok

--- assets/script.js
+++ assets/script.js
@@ -19,3 +19,58 @@ const slides = [
 ];
 
 
+const imageSlide = document.querySelector("#banner > img");
+const texteSlide = document.querySelector("#banner > p");
+const arrowLeftSlide = document.querySelector(".arrow_left");
+const arrowRightSlide = document.querySelector(".arrow_right");
+const totalSlides = slides.length;
+
+let indexSlides = 0;
+
+function animationSlide() {
+  imageSlide.src = `./assets/images/slideshow/${slides[indexSlides].image}`;
+  texteSlide.innerHTML = slides[indexSlides].tagLine;
+
+ const deleteNextDot = document.querySelector(".dot_selected");
+ if(deleteNextDot){
+  deleteNextDot.classList.remove("dot_selected");
+ }
+ let allDots = document.querySelectorAll(".dot");
+ let futurSelectedDot = allDots[indexSlides];
+ if(futurSelectedDot) {
+  futurSelectedDot.classList.add("dot_selected");
+ }
+}
+
+arrowLeftSlide.addEventListener("click", function() {
+  if(indexSlides == 0) {
+    indexSlides = totalSlides - 1;
+  } else {
+    indexSlides--;
+  }
+  animationSlide();
+})
+
+arrowRightSlide.addEventListener("click", function() {
+  if(indexSlides == totalSlides - 1) {
+    indexSlides = 0;
+  } else {
+    indexSlides++;
+  }
+  animationSlide();
+})
+
+function addNewDots() {
+  const dots = document.querySelector(".dots");
+  const dot = document.createElement("span")
+  dot.classList.add("dot")
+  dots.appendChild(dot)
+}
+
+for(let index = 0; index < slides.length; index++) {
+  addNewDots();
+}
+
+let firstDotSelected = document.querySelector(".dot");
+firstDotSelected.classList.add("dot_selected");
+
